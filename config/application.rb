9d067ebf1b91782136c1b506b3aff5ef555488b7
Ruben obregon <Ruben@unknown68a86d1ce81c.gateway.pace.com>
2014-06-15 13:55:27 -0500

    Added CSS v.1.1

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,7 @@ module Tdd1
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     #heroku
-    initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
+    
   end
 end
